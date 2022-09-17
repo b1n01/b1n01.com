ecd2b6d8f604dc679ee30cd0d3e66d94db2e28d4
Luca Lorenzini <chielorenz@gmail.com>
2022-09-17 12:35:42 +0200

    Prevent webpask to try to handle node "fs" import

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 // Config from https://github.com/vercel/next.js/tree/canary/examples/with-mdx
-
 const withMDX = require("@next/mdx")({
 	extension: /\.mdx?$/,
 });
 
 module.exports = withMDX({
 	pageExtensions: ["js", "jsx", "mdx"],
+	// Prevent webpask to try to handle node "fs" import
+	// @see https://stackoverflow.com/a/68098547/4851525
+	webpack: (config) => {
+		config.resolve.fallback = {
+			...config.resolve.fallback,
+			fs: false,
+		};
+		return config;
+	},
 });
