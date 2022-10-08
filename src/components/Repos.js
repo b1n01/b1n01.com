de1dcc57b3f1149cfc5559931e9ec583dbc2b077
b1n01 <chielorenzi@gmail.com>
2022-10-09 01:18:01 +0200

    Move login to fetch repos

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,43 +1,40 @@
 import { useEffect, useState } from "react";
 import style from "../style/Repos.module.css";
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
 const repoURL =
 	"https://api.github.com/users/b1n01/repos?sort=updated&direction=desc&page=1&per_page=4";
 
+const getRepos = async () => {
+	const repos = await fetch(repoURL).then((res) => res.json());
+	return await Promise.all(
+		repos.map(async ({ name, description, html_url, languages_url }) => {
+			const data = await fetch(languages_url).then((res) => res.json());
+			const langs = Object.keys(data);
+			return { name, desc: description, url: html_url, langs };
+		})
+	);
+};
+
 export default function App() {
 	const [repos, setRepos] = useState([]);
-	const [reposWithLangs, setReposWithLangs] = useState([]);
 
 	useEffect(() => {
-		fetcher(repoURL).then(setRepos);
+		getRepos().then(setRepos);
 	}, []);
 
-	useEffect(() => {
-		Promise.all(
-			repos.map(
-				async ({ name, description, html_url, languages_url }) => {
-					const data = await fetcher(languages_url);
-					const langs = Object.keys(data);
-					return { name, description, url: html_url, langs };
-				}
-			)
-		).then(setReposWithLangs);
-	}, [repos]);
-
-	if (!reposWithLangs) <p>Loading recent commits...</p>;
+	if (!repos) <p>Loading recent commits...</p>;
 
 	return (
 		<div className={style.wrapper}>
-			{reposWithLangs.map(({ name, description, url, langs }) => (
+			{repos.map(({ name, desc, url, langs }) => (
 				<a href={url} key={name} className={style.repo}>
 					<div className={style.title}>{name}</div>
-					<div className={style.description}>{description}</div>
+					<div className={style.description}>{desc}</div>
 					<div className={style.languages}>
-						{langs.map((label) => (
-							<div key={label}>
+						{langs.map((lang) => (
+							<div key={lang}>
 								<span className={style.dot} />
-								{label}
+								{lang}
 							</div>
 						))}
 					</div>
