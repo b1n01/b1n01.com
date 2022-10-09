fe1cbad36c8426df4abee17ae98ae73b97caeb7f
b1n01 <chielorenzi@gmail.com>
2022-10-10 00:16:59 +0200

    Add repos colors and cache api calls

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,18 +1,40 @@
 import { useEffect, useState } from "react";
 import style from "../style/Repos.module.css";
+import getLanguageColor from "../colors.js";
 
 const repoURL =
 	"https://api.github.com/users/b1n01/repos?sort=updated&direction=desc&page=1&per_page=4";
 
 const getRepos = async () => {
+	const cached = localStorage.getItem("repos");
+	if (cached) {
+		const data = JSON.parse(cached);
+		if (data.expiry > Date.now()) {
+			return data.repos;
+		}
+	}
+
 	const repos = await fetch(repoURL).then((res) => res.json());
-	return await Promise.all(
+	const reposWithLangs = await Promise.all(
 		repos.map(async ({ name, description, html_url, languages_url }) => {
-			const data = await fetch(languages_url).then((res) => res.json());
-			const langs = Object.keys(data);
+			const obj = await fetch(languages_url).then((res) => res.json());
+			const langs = Object.keys(obj).map((lang) => ({
+				lang,
+				color: getLanguageColor(lang),
+			}));
 			return { name, desc: description, url: html_url, langs };
 		})
 	);
+
+	localStorage.setItem(
+		"repos",
+		JSON.stringify({
+			repos: reposWithLangs,
+			expiry: Date.now() + 24 * 60 * 60 * 1000, // 1 day
+		})
+	);
+
+	return reposWithLangs;
 };
 
 export default function App() {
@@ -31,9 +53,12 @@ export default function App() {
 					<div className={style.title}>{name}</div>
 					<div className={style.description}>{desc}</div>
 					<div className={style.languages}>
-						{langs.map((lang) => (
-							<div key={lang}>
-								<span className={style.dot} />
+						{langs.map(({ lang, color }) => (
+							<div key={lang} className={style.dotWrapper}>
+								<span
+									className={style.dot}
+									style={{ backgroundColor: color }}
+								/>
 								{lang}
 							</div>
 						))}
