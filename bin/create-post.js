5718202643c6cee3a72245758e8d7cd0fded3776
Luca Lorenzini <chielorenz@gmail.com>
2022-10-11 18:43:15 +0200

    Sort posts by creation date

--- bin/create-post.js
+++ bin/create-post.js
@@ -4,14 +4,14 @@ import { writeFile } from "node:fs/promises";
 import path from "path";
 
 const title = process.argv.slice(2).join(" ");
-const time = new Date().toLocaleDateString("en-US");
+const time = Date.now();
 const slug = title.toLocaleLowerCase().split(" ").join("-");
 const file = path.join(process.cwd(), "posts", slug + ".mdx");
 
 const content = `---
 title: ${title}
 excerpt: Post excerpt here
-publishedOn: ${time}
+timestamp: ${time}
 ---
 
 # ${title}
--- src/postData.js
+++ src/postData.js
@@ -3,30 +3,31 @@ import { readdir, readFile } from "fs/promises";
 import matter from "gray-matter";
 import path from "path";
 
-// TODO sort posts by creation date (front matter attribute)
-
 export default async function getPostsData(config) {
 	const { limit } = config || {};
 
 	const postsPath = path.join(process.cwd(), "posts");
 	let files = await readdir(postsPath);
-	if (limit) files = files.slice(0, limit);
 
 	let posts = [];
 	for (const file of files) {
 		const filePath = path.join(process.cwd(), "posts", file);
-		const fileContents = await readFile(filePath, "utf8");
+		const fileContent = await readFile(filePath, "utf8");
 
-		const { content, data } = matter(fileContents);
+		const { content, data } = matter(fileContent);
 		const source = await serialize(content);
 
 		posts.push({
 			slug: path.parse(file).name,
 			title: data.title,
 			excerpt: data.excerpt,
+			timestamp: data.timestamp,
 			source,
 		});
 	}
+
+	posts.sort((a, b) => b.timestamp - a.timestamp);
+	if (limit) posts = posts.slice(0, limit);
 
 	return posts;
 }
